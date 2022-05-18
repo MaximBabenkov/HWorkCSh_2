2505b416d8eb8e3b25779953ae2190f7bf6b36fe
MaximBabenkov <lostbass@mail.ru>
2022-05-18 11:28:18 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+Console.Write("Enter X coordinate of the first point: ");
+int x1 = int.Parse(Console.ReadLine()??"0");
+Console.Write("Enter Y coordinate of the first point: ");
+int y1 = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Enter Z coordinate of the first point");
+Console.Write("If there is no Z coordinate - enter 0: ");
+int z1 = int.Parse(Console.ReadLine()??"0");
+Console.Write("Enter X coordinate of the second point: ");
+int x2 = int.Parse(Console.ReadLine()??"0");
+Console.Write("Enter Y coordinate of the second point: ");
+int y2 = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Enter Z coordinate of the second point");
+Console.Write("If there is no Z coordinate - enter 0: ");
+int z2 = int.Parse(Console.ReadLine()??"0");
+int sum = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1);
+double dist = Math.Sqrt(sum);
+Console.Write($"The distance between these points is: {Math.Round(dist,2)}");
