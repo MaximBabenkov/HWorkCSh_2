6c02aa6bba48c7ff675ebef29ab44ad95dd0fe04
MaximBabenkov <lostbass@mail.ru>
2022-05-18 11:18:32 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задать номер четверти, показать диапазон для возможных координат
+Console.Write("Enter your number of a quater: ");
+int a = int.Parse(Console.ReadLine()??"0");
+if(a>0&&a<=4)
+{
+    if(a==1) Console.WriteLine("The range of possible coordinates: X>0, y>0");
+    else if(a==2) Console.WriteLine("The range of possible coordinates: X<0, y>0");
+    else if(a==3) Console.WriteLine("The range of possible coordinates: X<0, y<0");
+    else if(a==4) Console.WriteLine("The range of possible coordinates: X>0, y<0");
+}
+else
+{
+    Console.WriteLine("Your number of a quater is incorrect");
+}
