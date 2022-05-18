714795d9045a63c4c2fc7c223d08c9b0c2e7ec56
MaximBabenkov <lostbass@mail.ru>
2022-05-18 10:57:19 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Дано число, обозначающее день недели.
+// Выяснить - является ли это день выходным 
+Console.WriteLine("Enter your number of a day of week");
+int a = int.Parse(Console.ReadLine()??"0");
+if (a==6||a==7)
+{
+    Console.WriteLine("It's a day off work");
+}
+else
+{
+    Console.WriteLine("It's a working day");
+}
