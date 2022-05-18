5e54faa717ed7471c06fdd7fdb7746e5711eab7d
MaximBabenkov <lostbass@mail.ru>
2022-05-18 11:04:29 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// По двум заданным числам определить - является ли одно квадратом другого
+Console.WriteLine("Enter your first number");
+int a = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Enter your second number");
+int b = int.Parse(Console.ReadLine()??"0");
+if(a==b*b||b==a*a) 
+{
+    Console.WriteLine("One number is a square of the other number");
+}
+else
+{
+    Console.WriteLine("One number is not a square of the other number");
+}
